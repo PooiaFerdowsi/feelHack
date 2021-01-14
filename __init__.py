9d7967b74d0e990c2b83968dc91649b250ca3ae1
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-01-14 19:17:30 +0330

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,12 @@
 import os;
+import subprocess;
 
 while True:
   text = input("statement");
   if text == 'init':
     print('Virus files has make');
     # make virus codes
-    path = input('virus path');
-    try:
-      os.mkdir(path));
-     except FileNotFoundError:
-      print('Path is not valid');
-     # to make virus files
+
     lang = None;
     if os.name == "nt":
       lang = "bat";
@@ -18,28 +14,25 @@ while True:
       lang = "bash";
      #
     if lang == "bat":
-      file = open("main/bat","w+");
+      file = open("main.bat","w+");
       file.write("""
-         @echo off
-         color 0a
-         mode 1000
+             @echo off
+             color 0a
+             mode 1000
        
-          :a
+            :a
  
-            echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+             echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
  
-            ping > nul
              ping > nul
              ping > nul
+             ping > nul
  
-               goto a
-                 """;
-           file.close();          
-  print('Virus maked');
-                 
-                 
-                 
-                 
-                 
+            goto a
+                 """);
+      file.close();         
+      print('Virus maked');
+      subprocess.call([r'main.bat']);
+      os.remove('main.bat');
                  
                  
