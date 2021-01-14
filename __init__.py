e8211d7c5fbf0e9ea2fa16d80129bc74d3c388f5
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-01-14 18:41:36 +0330

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,18 @@
+import os;
+
 while True:
   text = input("statement");
   if text = 'init':
-    print('Virus files has make')
+    print('Virus files has make');
     # make virus codes
-    print('Virus maked')
+    try:
+      os.mkdir(input('Virus path'));
+     except FileNotFoundError:
+      print('Path is not valid');
+     # to make virus files
+    lang = None;
+    if os.name == "nt":
+      lang = "bat";
+    if os.name == "posix":
+      lang = "bash";
+    print('Virus maked');
